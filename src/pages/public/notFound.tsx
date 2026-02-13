d20801be7e8f797d8b94124a948260644fe6ba5b
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2026-02-13 11:35:10 -0300

    ✨ create not found page

--- src/pages/public/notFound.tsx
+++ src/pages/public/notFound.tsx
@@ -1,5 +1,45 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { Box, Button, Stack, Text, Title } from "@mantine/core";
+import { useNavigate } from "react-router";
+import { ThemeToggle } from "../../components";
+
 const PageNotFound = () => {
-	return <p>PageNotFound</p>;
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate("/");
+  };
+  return (
+    <Box className="bg-background h-screen w-screen relative flex flex-col items-center justify-center">
+      <Box pos="absolute" top={20} right={20}>
+        <ThemeToggle />
+      </Box>
+      <Box p="xl">
+        <Stack align="center" mb="lg">
+          <Box maw={600}>
+            <Title fw={800} ta="center" className="text-text-main">
+              Página não encontrada
+            </Title>
+            <Text size="md" ta="center" fw={500} mt={4} className="text-primary">
+              A página que você está tentando acessar não foi encontrada.
+            </Text>
+          </Box>
+        </Stack>
+      </Box>
+      <Box className="w-[600px]">
+        <DotLottieReact src="src/assets/animations/404.lottie" speed={0.4} loop autoplay />
+      </Box>
+      <Button
+        mt="lg"
+        onClick={handleGoBack}
+        className="bg-primary hover:bg-primary/90 text-primary-foreground"
+        radius="md"
+        size="md"
+      >
+        Voltar para a página inicial
+      </Button>
+    </Box>
+  );
 };
 
 export default PageNotFound;
