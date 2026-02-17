9ddb59c4d135ecfb73a9bdde74896f8a0b846066
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2026-02-17 15:28:38 -0300

    ♻️ update components exporter

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,17 @@
+import { AlertFeed } from "./AlertFeed";
+import { ChartTooltip } from "./ChartTooltip";
+import { ConsumptionChart } from "./ConsumptionChart";
+import { RecentMovements } from "./RecentMovements";
+import { Sidebar } from "./Sidebar";
+import { StatCard } from "./StatCard";
 import { ThemeToggle } from "./ThemeToggle";
 
-export { ThemeToggle };
+export {
+  ThemeToggle,
+  Sidebar,
+  StatCard,
+  ConsumptionChart,
+  AlertFeed,
+  RecentMovements,
+  ChartTooltip,
+};
