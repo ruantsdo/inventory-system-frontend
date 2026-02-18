d3d92c6a7a63ff1ca722ff56b4130cf097be9d01
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2026-02-18 14:06:16 -0300

    ♻️ update login schema

--- src/schemas/auth/login.ts
+++ src/schemas/auth/login.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-	cpf: z
-		.string()
-		.min(1, "CPF obrigatório")
-		.transform((val) => val.replace(/\D/g, ""))
-		.refine((val) => val.length === 11, {
-			message: "CPF incompleto",
-		}),
+  credential: z
+    .string()
+    .min(1, "CPF obrigatório")
+    .transform((val) => val.replace(/\D/g, ""))
+    .refine((val) => val.length === 11, {
+      message: "CPF incompleto",
+    }),
 
-	password: z.string().min(4, "Senha obrigatória"),
-	rememberMe: z.boolean(),
+  password: z.string().min(4, "Senha obrigatória"),
+  rememberMe: z.boolean(),
 });
 
 export type LoginRequest = z.infer<typeof loginSchema>;
