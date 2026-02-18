a58f2518d948e6f270210a4de4a7f9f0c65f0f81
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2026-02-18 14:14:36 -0300

    🔧 implement redirect and notifications

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,33 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+
 import { RouterProvider } from "react-router";
 import { router } from "./routes/";
 
+import { useAuthStore } from "./stores/auth";
+
+const App = () => {
+  const hasCheckedAuth = useAuthStore((state) => state.hasCheckedAuth);
+
+  if (!hasCheckedAuth) {
+    return null;
+  }
+
+  return <RouterProvider router={router} />;
+};
+
+useAuthStore.getState().checkAuth();
+
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<MantineProvider>
-			<RouterProvider router={router} />
-		</MantineProvider>
-	</StrictMode>,
+  <StrictMode>
+    <MantineProvider>
+      <Notifications />
+      <App />
+    </MantineProvider>
+  </StrictMode>,
 );
