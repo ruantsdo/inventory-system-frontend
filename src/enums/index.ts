51999c0165a98d760ecb5993f28bb11f2538eb25
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2026-05-01 12:23:06 -0300

    feat: centralize static lookup data and add permission display names

---
+++ src/enums/index.ts
@@ -0,0 +1,9 @@
+export { brazilianStates } from "./brazilianStates";
+export { professionalDocumentTypes } from "./documents";
+export type {
+  AppPermissionKey,
+  PermissionKey,
+  PermissionScopeMode,
+  UserRoleScopeMode,
+} from "./permissions";
+export { permissionDisplayNames, permissionKeys } from "./permissions";
